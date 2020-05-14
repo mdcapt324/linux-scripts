65ce00eb8ff35ae6c6d295f3497dd25d1e9c7cff
Michael Clement <mclement@tushka.k12.ok.us>
2020-05-14 08:31:23 -0500

    Updated package list

--- packages.sh
+++ packages.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
 
-sudo pacman -S arcolinux-teamviewer geany geany-plugins libreoffice-still audacity deadbeef \
-	rofi galculator screenkey-git pcmanfm ranger alacritty imagewriter xarchiver \
-	powerline powerline-fonts vifm
+sudo pacman -S \
+	arcolinux-teamviewer \
+	geany \
+	geany-plugins \
+	libreoffice-still \
+	audacity deadbeef \
+	rofi \
+	galculator \
+	screenkey-git \
+	pcmanfm \
+	ranger \
+	alacritty \
+	imagewriter \
+	xarchiver \
+	powerline \
+	powerline-fonts \
+	vifm
